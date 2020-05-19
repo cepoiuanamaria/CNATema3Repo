fb264d3a6648b1d199c13d5279ac917a268a6741
Beatris <heresbeatriselena@gmail.com>
2020-05-19 14:28:02 +0300

    Commit 2
    Am creat partea de protobuf, server si primul utilizator.

---
+++ Hello/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Host = "localhost";
+            const int Port = 16842;
+
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+            var client = new Generated.MessageReply.MessageReplyClient(channel);
+            var response = client.SayHello(new Generated.MessageRequest{
+                Name = Console.ReadLine(),
+            });
+
+            // Shutdown
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Hello/Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Server server = new Server(Configuration.HOST, Configuration.PORT))
+            {
+                server.CloseServerAction = () => Console.ReadKey();
+                server.Start();
+            }
+        }
+    }
+}
+
+
