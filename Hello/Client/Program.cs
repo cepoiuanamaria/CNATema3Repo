f1f508d40fa77d64cbfdd0ce4bf3242207281e21
CepoiuAnaMaria <cepoiuanamaria@yahoo.com>
2020-05-31 12:44:16 +0300

    conectarea clientului la server folosind chat.proto

--- Hello/Client/Program.cs
+++ Hello/Client/Program.cs
@@ -3,27 +3,71 @@ using Grpc.Net.Client;
 using Server;
 using Server.Protos;
 using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    Console.Title="Client";
+        //    var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+        //    var client = new MessageReply.MessageReplyClient(channel);
+
+        //    Console.Write("Name: ");
+
+        //    var response = client.SayHello(new MessageRequest
+        //    {
+        //        Name = Console.ReadLine(),
+        //    });
+        //    Console.WriteLine(response.Message);
+
+        //    // Shutdown
+        //    channel.ShutdownAsync().Wait();
+        //    Console.WriteLine("Press any key to exit...");
+        //    Console.ReadKey();
+        //}
+        static async Task Main(string[] args)
         {
-            Console.Title="Client";
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            Console.Title = "Client";
+            Console.WriteLine("Username: ");
+            var userName = Console.ReadLine();
+            //Include portul serverului grpc ca si argument al aplicatiei
+            var port = args.Length > 0 ? args[0] : "5001";
 
-            var client = new MessageReply.MessageReplyClient(channel);
+            var channel = GrpcChannel.ForAddress("https://localhost:" + port);
+            var client = new ChatRoom.ChatRoomClient(channel);
 
-            Console.Write("Name: ");
-
-            var response = client.SayHello(new MessageRequest
+            using (var chat =client.join())
             {
-                Name = Console.ReadLine(),
-            });
-            Console.WriteLine(response.Message);
+                _ = Task.Run(async () =>
+                  {
+                      while (await chat.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
+                      {
+                          var response = chat.ResponseStream.Current;
+                          Console.WriteLine($"{response.User}: {response.Text}");
+                      }
+                  });
 
-            // Shutdown
+                await chat.RequestStream.WriteAsync(new Message { User = userName, Text = $"{userName} has joined the room" });
+                string line;
+                while((line=Console.ReadLine())!=null)
+                {
+                    if (line.ToLower() == "bye")
+                        break;
+                    await chat.RequestStream.WriteAsync(new Message { User = userName, Text = line });
+
+                }
+                await chat.RequestStream.CompleteAsync();
+            }
+            Console.WriteLine("Disconnecting");
             channel.ShutdownAsync().Wait();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
--- Hello/Server/Services/ChatOperationService.cs
+++ Hello/Server/Services/ChatOperationService.cs
@@ -7,35 +7,35 @@ using System.Threading.Tasks;
 
 namespace Server.Services
 {
-    internal class ChatOperationService : MessageReply.MessageReplyBase
+    //internal class ChatOperationService : MessageReply.MessageReplyBase
+    //{
+    public class ChatOperationService : ChatRooms.ChatRoomsBase
     {
-        //public class ChatOperationService: ChatRooms.ChatRoomsBase
-        //{
-        //    private readonly Server.ChatRoom _chatroomService;
+        private readonly Server.ChatRoom _chatroomService;
 
-        //    public ChatOperationService(Server.ChatRoom chatRoomService)
-        //    {
-        //        _chatroomService = chatRoomService;
-        //    }
-
-        //    public override async Task join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
-        //    {
-        //        if (!await requestStream.MoveNext())
-        //            return;
-        //        do
-        //        {
-        //            _chatroomService.Join(requestStream.Current.User, responseStream);
-        //            await _chatroomService.BroadcastMessageAsync(requestStream.Current);
-        //        } while (await requestStream.MoveNext());
-        //        _chatroomService.Remove(context.Peer);
-        //    }
-        public override Task<OperationResponse> SayHello(MessageRequest request, ServerCallContext context)
+        public ChatOperationService(Server.ChatRoom chatRoomService)
         {
-            System.Console.WriteLine("wazzup my nigga " + request.Name);
-
-            var message = "";
-
-            return Task.FromResult(new OperationResponse() { Message = message });
+            _chatroomService = chatRoomService;
         }
+
+        public override async Task join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext())
+                return;
+            do
+            {
+                _chatroomService.Join(requestStream.Current.User, responseStream);
+                await _chatroomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+            _chatroomService.Remove(context.Peer);
+        }
+        //public override Task<OperationResponse> SayHello(MessageRequest request, ServerCallContext context)
+        //{
+        //    System.Console.WriteLine("wazzup my nigga " + request.Name);
+
+        //    var message = "";
+
+        //    return Task.FromResult(new OperationResponse() { Message = message });
+        //}
     }
 }
